73b94b0d3badc20573f8a866aeb73ef4dd8600a6
pixelwhisperer <steveyawan@Steves-iMac.local>
2016-12-19 15:51:53 -0800

    Tuts so far

--- react-stuff/react-redux/src/routes.js
+++ react-stuff/react-redux/src/routes.js
@@ -3,10 +3,12 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/homePage';
 import AboutPage from './components/about/aboutPage';
+import CoursesPage from './components/course/CoursesPage';
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={HomePage } />
+    <IndexRoute component={HomePage} />
+    <Route path="courses" component={CoursesPage} />
     <Route path="about" component={AboutPage} />
   </Route>
 )
