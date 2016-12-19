f1e0b53ffa75b6c70dad01432ecd0cbc77abba61
pixelwhisperer <steveyawan@Steves-iMac.local>
2016-12-19 14:27:35 -0800

    Added tut files

---
+++ react-stuff/react-redux/src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+import App from './components/App';
+import HomePage from './components/home/homePage';
+import AboutPage from './components/about/aboutPage';
+
+export default (
+  <Route path="/" component={App}>
+    <IndexRoute component={HomePage } />
+    <Route path="about" component={AboutPage} />
+  </Route>
+)
